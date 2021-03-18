c8dd53545b73ea2d005fdd47c48a4a25321b469e
Reed Spool <reedwith2es@gmail.com>
2021-03-17 22:42:01 -0600

    Implement initial load of a group of pokemon and subsequent load of data

--- core/CoreMachine.js
+++ core/CoreMachine.js
@@ -1,19 +1,75 @@
 import { Machine } from "xstate";
 import { assign } from '@xstate/immer';
+import { getIdFromUrl, getOne, getRandomGroup } from "./PokemonAPI.js";
+const AMOUNT = 50;
 
 export const definition = {
     strict: true,
     id: "core",
-    initial: "uninitialized",
+    initial: "loadingRandom",
     states: {
-        uninitialized: {}
+        loadingRandom: {
+            invoke : {
+                id: "fetchRandom",
+                src: "fetchRandom",
+                onDone: {
+                    target: "loadingIndividuals",
+                    actions: ["recordGroup"]
+                },
+                onError: {
+                    target: "failure",
+                    actions: () => alert("There was a problem with the Pokemon API :(")
+                }
+            },
+        },
+        loadingIndividuals: {
+            invoke : {
+                id: "fetchIndividuals",
+                src: "fetchIndividuals",
+                onDone: {
+                    target: "active",
+                    actions: ["recordIndividuals"]
+                },
+                onError: {
+                    target: "failure",
+                    actions: () => alert("There was a problem with the Pokemon API :(")
+                }
+            }
+        },
+        active: {},
+        failure: { type: "final" }
     },
     on: {}
 };
 
 export const config = {
-    actions: {},
-    guards : {}
+    actions: {
+        recordGroup: assign((C, E) => {
+            C.group = E.data.results;
+            C.individualsById = {};
+
+            // Transform the list of { url, name } into
+            // a map of id => { name }
+            E.data.results.forEach(
+                ({ url, name }) =>
+                    C.individualsById[getIdFromUrl(url)] = { name });
+        }),
+        recordIndividuals: assign((C, E) => {
+            // Unpack into existing skeleton
+            E.data.forEach(({ id, sprites }) => {
+                C.individualsById[id].sprites = sprites;
+            })
+        })
+    },
+    guards : {},
+    services: {
+        fetchRandom : () => getRandomGroup(AMOUNT),
+        fetchIndividuals : (C, E) => {
+            return Promise.all(
+                E.data.results.map(({ url }) =>
+                    getOne(getIdFromUrl(url))));
+        }
+    }
 };
 
 export const init = () => Machine(definition, config);
--- test/core/CoreMachine.test.js
+++ test/core/CoreMachine.test.js
@@ -1,7 +1,68 @@
 import * as CoreMachine from "../../core/CoreMachine";
+import { Machine } from "xstate";
+import { toMatchState } from "../xstate-jest-matchers";
+import { individualsByName, group, individualsById } from "./PokemonMockData";
+import { getIdFromUrl } from "../../core/PokemonAPI";
+expect.extend({ toMatchState });
 
 describe("Basic CoreMachine", () => {
+    let machine;
+    let S;
+    let C;
+
+    function transition(event) {
+        S = machine.transition(S, event);
+        C = S.context;
+    }
+
+    beforeEach(() => {
+        machine = Machine(CoreMachine.definition)
+            .withConfig(CoreMachine.config)
+            // Supply empty context to avoid warning
+            .withContext({});
+
+        S = machine.initialState;
+        C = S.context;
+    });
+
     it("Can be imported", () => {
         expect(CoreMachine).toBeDefined();
+    })
+
+    it("Immediately gets group of pokemon", () => {
+        expect(S).toMatchState("loadingRandom");
+    })
+
+    it("Responds to successful load of group", () => {
+        // Simulate the resolving of the promise
+        transition({
+            type: 'done.invoke.fetchRandom',
+            data: group
+        })
+        expect(S).toMatchState("loadingIndividuals");
+        expect(C.group).toEqual(group.results);
+
+        // The results should be unpacked from an array of {url,name}
+        // to a map of id => { name }, to be filled in later
+        const id = getIdFromUrl(C.group[0].url);
+        expect(C.individualsById).toHaveProperty(""+id);
+        expect(C.individualsById[id].name).toBe(C.group[0].name);
+    })
+
+    it("Responds to successful load of items", () => {
+        // Simulate the resolving of the promise
+        transition({
+            type: 'done.invoke.fetchRandom',
+            data: group
+        })
+        transition({
+            type: 'done.invoke.fetchIndividuals',
+            data: Object.values(individualsById)
+        })
+        expect(S).toMatchState("active");
+
+        // The results of the individual requests should now be unpacked into
+        // the existing individualsById skeleton
+        expect(C.individualsById[171]).toHaveProperty("sprites");
     })
 })
